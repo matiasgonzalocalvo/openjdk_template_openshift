703a6c4e429f1cd666e2056d8cd91045637f8675
Matias Gonzalo Calvo <matias.calvo@comafi.com.ar>
2019-05-07 12:22:41 -0300

    Jenkinsfile

--- function.groovy
+++ function.groovy
@@ -1,17 +1,12 @@
-def aws_config() {
+def aws_config(credential_id, def AWS_ACCESS_KEY_ID="AWS_ACCESS_KEY_ID", def AWS_SECRET_ACCESS_KEY="AWS_SECRET_ACCESS_KEY") {
     withCredentials([[
-    $class: 'AmazonWebServicesCredentialsBinding',
-    credentialsId: 'AWS_DESA',
-    accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-    secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+    $class: "AmazonWebServicesCredentialsBinding",
+    credentialsId: "${credential_id}",
+    accessKeyVariable: "${AWS_ACCESS_KEY_ID}",
+    secretKeyVariable: "${AWS_SECRET_ACCESS_KEY}"
     ]]) {
-        sh 'AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
-        AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
-        AWS_REGION=us-east-1'
-        sh "aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}"
-        sh "aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}"
-        sh "aws configure set region ${AWS_REGION}"
-        sh "aws configure list --profile default"
+        evaluate "env.${AWS_ACCESS_KEY_ID}=${AWS_ACCESS_KEY_ID}"
+        evaluate "env.${AWS_SECRET_ACCESS_KEY}=${AWS_SECRET_ACCESS_KEY}"
     }
 }
 def send_slack(def estado=null,def emoji="ghost",def channel="#jenkins",def text="Job $JOB_NAME Build number $BUILD_NUMBER for branch $BRANCH_NAME has",def slackurl="https://hooks.slack.com/services/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") {
