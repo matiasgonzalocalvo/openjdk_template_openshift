d89077fd8fa5e6a537dd7eacc330e9957ffac063
Matias Gonzalo Calvo <matias.calvo@comafi.com.ar>
2019-05-09 11:06:28 -0300

    pom.xml

--- function.groovy
+++ function.groovy
@@ -19,8 +19,25 @@ def update_aws_credentials(UPDATE_SERVICE_KEY_ID,UPDATE_SERVICE_SECRET_KEY)
     env.AWS_ACCESS_KEY_ID = "${UPDATE_SERVICE_KEY_ID}"
     env.AWS_SECRET_ACCESS_KEY = "${UPDATE_SERVICE_SECRET_KEY}"    
 }
-def mavane_verify(def settings="null")
+def maven_verify(def settings="null")
 {
     sh "mvn verify -DskipTests -s ${settings} -X"
 }
+def maven_sonar(def settings="null", def sonar_url="null", def sonar_login="null")
+{
+    sh "mvn sonar:sonar -DskipTests -s ${settings} -X -Dsonar.host.url=${sonar_url} -Dsonar.login=${sonar_login}"
+}
+def maven_deploy(def settings="null")
+{
+    sh "mvn deploy -DskipTests -s ${settings} -X"
+}
+def docker_build(def name="null",def tag="null",def url_docker_tcp="null")
+{
+    sh 'docker -H "${url_docker_tcp}" build -t ${name}:${tag} .'
+}
+def docker_push(def name="null",def tag="null",def url_docker_tcp="null")
+{
+    sh 'docker -H "${url_docker_tcp}" push -t ${name}:${tag} .'
+}
+
 return this
