94d36923566757b39d8ffc5027379a675d4af243
Matias Gonzalo Calvo <matias.calvo@comafi.com.ar>
2019-05-24 10:28:52 -0300

    fix tag 2

--- function.groovy
+++ function.groovy
@@ -219,7 +219,7 @@ def git_tag(def credentials="devops-bitbucket")
     passwordVariable: 'GIT_PASS', usernameVariable: 'GIT_USER')]) {
     sh """
       git_url=`echo ${GIT_URL} | sed 's"http://""g'|sed 's"https://""g'`
-      git tag `echo $BRANCH_NAME | cut -d "/" -f2`.$BUILD_NUMBER'
+      git tag `echo $BRANCH_NAME | cut -d "/" -f2`.$BUILD_NUMBER
       git push https://devops:$GIT_PASS@${git_url} --tags
       echo "test export git_tag"
       export
