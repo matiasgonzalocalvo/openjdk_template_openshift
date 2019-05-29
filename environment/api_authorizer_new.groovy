44197946f75ba8874a4bca09f6d7552159d7a22c
Matias Gonzalo Calvo <matias.calvo@comafi.com.ar>
2019-05-29 12:37:34 -0300

    test multicuenta

--- environment/api_authorizer_new.groovy
+++ environment/api_authorizer_new.groovy
@@ -11,41 +11,94 @@ def setenv(def cuenta="null")
     */    
     if (env.BRANCH_NAME == "master" || env.BRANCH_NAME == "prod")
     {
-        devops.aws_config("_srv_jenkins_pec")
+        //devops.aws_config("_srv_jenkins_pec")
         sh 'echo "$(date) : Seteando variables - BRANCH = ${BRANCH_NAME}"'
         env.tag="true"
         env.ENV='prod'
         env.COST_CENTER='comafi_digital_prod'
         env.FILES_BUCKET='cmf-comercios-sec-files'
         envSTACK_NAME='Authorizer'
+        if ( cuenta == "_srv_jenkins_pec" )
+        {
+          devops.aws_config("_srv_jenkins_pec")
+          return true
+        }
+        else
+        {
+          return false
+        }
     }
     else if (env.BRANCH_NAME == "develop" || env.BRANCH_NAME == "developjenkinsfile")
     {
         sh 'echo "$(date) : Seteando variables - BRANCH = ${BRANCH_NAME}"'
-        devops.aws_config("AWS_DESA_CMF")
+        //devops.aws_config("AWS_DESA_CMF")
         env.ENV='dev'
         env.COST_CENTER='comercios_dev'
         env.FILES_BUCKET='cmf-comercios-sec-files'
         env.STACK_NAME='Authorizer'
+        if ( cuenta == "null" || cuenta == "AWS_DESA_CMF" )
+        {
+          echo "cuenta == ${cuenta} "
+          devops.aws_config("AWS_DESA_CMF")
+          return true
+        }
+        else if ( cuenta == "AWS_DESA" )
+        {
+          echo "cuenta == ${cuenta} "
+          devops.aws_config("AWS_DESA")
+          return true
+        }
+        else
+        {
+          return false
+        }
     }
     else if (env.BRANCH_NAME =~ "feature/*" || env.BRANCH_NAME =~ "PR*" )
     {
         sh 'echo "$(date) : Seteando variables - BRANCH = ${BRANCH_NAME}"'
-        devops.aws_config("AWS_DESA_CMF")
+        //devops.aws_config("AWS_DESA_CMF")
         env.ENV='predev'
         env.COST_CENTER='comercios_dev'
         env.FILES_BUCKET='cmf-comercios-sec-files'
         env.STACK_NAME='Authorizer'
+        if ( cuenta == "null" || cuenta == "AWS_DESA_CMF" )
+        {
+          devops.aws_config("AWS_DESA_CMF")
+          return true
+        }
+        else if ( cuenta == "AWS_DESA" )
+        {
+          devops.aws_config("AWS_DESA")
+          return true
+        }
+        else
+        {
+          return false
+        }
     }
     else if (env.BRANCH_NAME == "impleqa" || env.BRANCH_NAME == "qa" || env.BRANCH_NAME =~ "release/*" )
     {
         sh 'echo "$(date) : Seteando variables - BRANCH = ${BRANCH_NAME}"'
-        devops.aws_config("AWS_DESA_CMF")
+        //devops.aws_config("AWS_DESA_CMF")
         env.tag="true"
         env.ENV='qa'
         env.COST_CENTER='comafi_digital_qa'
         env.FILES_BUCKET='cmf-comercios-sec-files'
         env.STACK_NAME='Authorizer'
+        if ( cuenta == "null" || cuenta == "AWS_DESA_CMF" )
+        {
+          devops.aws_config("AWS_DESA_CMF")
+          return true
+        }
+        else if ( cuenta == "AWS_DESA" )
+        {
+          devops.aws_config("AWS_DESA")
+          return true
+        }
+        else
+        {
+          return false
+        }
     }
     else if ( env.BRANCH_NAME =~ "test-flow" )
     {
@@ -72,7 +125,7 @@ def setenv(def cuenta="null")
         {
           return false
         }
-      echo "llego al final no deberia llegar"
+      //echo "llego al final no deberia llegar"
     }
     else
     {
