4ff6e97fb6dd56c8ccd63916c28585eadc72d88d
Matias Gonzalo Calvo <matias.calvo@comafi.com.ar>
2019-06-05 16:45:12 -0300

    add count aws hbi

--- environment/comafi-hbi-front.groovy
+++ environment/comafi-hbi-front.groovy
@@ -15,6 +15,7 @@ def setenv(def cuenta="null")
   {
     sh 'echo "$(date) : Seteando variables - BRANCH = ${BRANCH_NAME}"'
     env.ENV="prod"
+    devops.aws_config("AWS_Alternative")
   }
   else if (env.BRANCH_NAME == "impleqa" || env.BRANCH_NAME == "qa" || env.BRANCH_NAME =~ "release/*" )
   {
@@ -39,15 +40,18 @@ def setenv(def cuenta="null")
     env.ENV="${ENV}"
     //sh "export"
     echo "you selected: ${ENV}"
+    devops.aws_config("AWS_DESA")
   }
   else if (env.BRANCH_NAME == "develop" || env.BRANCH_NAME == "developjenkinsfile" )
   {
     sh 'echo "$(date) : Seteando variables - BRANCH = ${BRANCH_NAME}"'
     env.ENV="dev"
+    devops.aws_config("AWS_DESA")
   }
   else if (env.BRANCH_NAME =~ "feature/*" || env.BRANCH_NAME =~ "PR*")
   {
     sh 'echo "$(date) : Seteando variables - BRANCH = ${BRANCH_NAME}"'
+    devops.aws_config("AWS_DESA")
     env.ENV="sandbox"
     //set_npm_nexus()
   }
