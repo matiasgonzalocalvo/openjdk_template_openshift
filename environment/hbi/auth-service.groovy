c45e16e9ecd77f9f43e51822fa1c19f6514713dd
calvomatias <matias.calvo@comafi.com.ar>
2019-06-24 14:12:23 -0300

    add devops.aws_config(705437fe-118c-4fbc-af26-595cbdc1e752)

--- environment/hbi/auth-service.groovy
+++ environment/hbi/auth-service.groovy
@@ -28,6 +28,7 @@ def setenv(def cuenta="null")
   else if (env.BRANCH_NAME == "impleqa" || env.BRANCH_NAME == "qa" || env.BRANCH_NAME =~ "release/*" )
   {
     sh 'echo "$(date) : Seteando variables - BRANCH = ${BRANCH_NAME}"'
+    devops.aws_config("705437fe-118c-4fbc-af26-595cbdc1e752")
     devops.config_file_provider("9bca46b5-370c-4e9c-97bf-447123727106")
     env.ECR_URL=104455529394.dkr.ecr.us-east-1.amazonaws.com
     env.ECR_ID=auth
@@ -46,6 +47,7 @@ def setenv(def cuenta="null")
   else if (env.BRANCH_NAME == "develop" || env.BRANCH_NAME == "developjenkinsfile" )
   {
     sh 'echo "$(date) : Seteando variables - BRANCH = ${BRANCH_NAME}"'
+    devops.aws_config("705437fe-118c-4fbc-af26-595cbdc1e752")
     devops.config_file_provider("9bca46b5-370c-4e9c-97bf-447123727106")
     env.ENVNAME="dev"
     env.aws_key_1="705437fe-118c-4fbc-af26-595cbdc1e752"
@@ -69,6 +71,7 @@ def setenv(def cuenta="null")
   else if (env.BRANCH_NAME =~ "feature/*" || env.BRANCH_NAME =~ "PR*")
   {
     sh 'echo "$(date) : Seteando variables - BRANCH = ${BRANCH_NAME}"'
+    devops.aws_config("705437fe-118c-4fbc-af26-595cbdc1e752")
     devops.config_file_provider("9bca46b5-370c-4e9c-97bf-447123727106")
     env.ENVNAME="sandbox"
     env.aws_key_1="705437fe-118c-4fbc-af26-595cbdc1e752"
