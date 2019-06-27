3da54b6ed35753d38dce93805e15287e67f55a29
calvomatias <matias.calvo@comafi.com.ar>
2019-06-27 17:50:57 -0300

    fix predev alta-de-tarjetas

--- environment/surf/alta-de-tarjetas-front.groovy
+++ environment/surf/alta-de-tarjetas-front.groovy
@@ -28,7 +28,7 @@ def setenv(def cuenta="null")
   else if (env.BRANCH_NAME == "impleqa" || env.BRANCH_NAME == "qa" || env.BRANCH_NAME =~ "release/*" )
   {
     sh 'echo "$(date) : Seteando variables - BRANCH = ${BRANCH_NAME}"'
-    devops.aws_config("AWS_Alternative")
+    devops.aws_config("AWS_DESA_CMF")
     env.ENV='qa'
     env.DOMAIN=""
     def verCode = UUID.randomUUID().toString()
@@ -42,7 +42,7 @@ def setenv(def cuenta="null")
   else if (env.BRANCH_NAME == "develop" || env.BRANCH_NAME == "developjenkinsfile" )
   {
     sh 'echo "$(date) : Seteando variables - BRANCH = ${BRANCH_NAME}"'
-    devops.aws_config("AWS_Alternative")
+    devops.aws_config("AWS_DESA_CMF")
     env.ENV='dev'
     env.DOMAIN=""
     def verCode = UUID.randomUUID().toString()
@@ -56,7 +56,7 @@ def setenv(def cuenta="null")
   else if (env.BRANCH_NAME =~ "feature/*" || env.BRANCH_NAME =~ "PR*")
   {
     sh 'echo "$(date) : Seteando variables - BRANCH = ${BRANCH_NAME}"'
-    devops.aws_config("AWS_Alternative")
+    devops.aws_config("AWS_DESA_CMF")
     env.ENV='dtkpredev'
     def verCode = UUID.randomUUID().toString()
     env.random="${verCode}"
