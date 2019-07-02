03d8cfbe3927ddbaf09ce0372aa9f08a9b236b27
calvomatias <matias.calvo@comafi.com.ar>
2019-07-02 12:44:37 -0300

    fix settings js-parent-commons.groovy

--- environment/hbi/js-parent-commons.groovy
+++ environment/hbi/js-parent-commons.groovy
@@ -16,6 +16,7 @@ def setenv(def cuenta="null")
   if (env.BRANCH_NAME == "master" || env.BRANCH_NAME == "prod")
   {
     sh 'echo "$(date) : Seteando variables - BRANCH = ${BRANCH_NAME}"'
+    devops.config_file_provider("9bca46b5-370c-4e9c-97bf-447123727106")
     env.maven_verify="true"
     env.maven_cobertura="true"
     env.maven_sonar="true"
@@ -24,6 +25,7 @@ def setenv(def cuenta="null")
   else if (env.BRANCH_NAME == "impleqa" || env.BRANCH_NAME == "qa" || env.BRANCH_NAME =~ "release/*" )
   {
     sh 'echo "$(date) : Seteando variables - BRANCH = ${BRANCH_NAME}"'
+    devops.config_file_provider("9bca46b5-370c-4e9c-97bf-447123727106")
     env.maven_verify="true"
     env.maven_cobertura="true"
     env.maven_sonar="true"
@@ -32,6 +34,7 @@ def setenv(def cuenta="null")
   else if (env.BRANCH_NAME == "develop" || env.BRANCH_NAME == "developjenkinsfile" )
   {
     sh 'echo "$(date) : Seteando variables - BRANCH = ${BRANCH_NAME}"'
+    devops.config_file_provider("9bca46b5-370c-4e9c-97bf-447123727106")
     env.maven_verify="true"
     env.maven_cobertura="true"
     env.maven_sonar="true"
@@ -40,6 +43,7 @@ def setenv(def cuenta="null")
   else if (env.BRANCH_NAME =~ "feature/*" || env.BRANCH_NAME =~ "PR*")
   {
     sh 'echo "$(date) : Seteando variables - BRANCH = ${BRANCH_NAME}"'
+    devops.config_file_provider("9bca46b5-370c-4e9c-97bf-447123727106")
     env.maven_verify="true"
     env.maven_cobertura="true"
     env.maven_sonar="true"
