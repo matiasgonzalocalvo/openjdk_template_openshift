4529c8648b8430245636f452e91e18ddcb9a3e8e
calvomatias <matias.calvo@comafi.com.ar>
2019-07-10 14:22:22 -0300

    fix flujo dockerbuild

--- environment/docker/arch-jenkins-slave-centos.groovy
+++ environment/docker/arch-jenkins-slave-centos.groovy
@@ -1,6 +1,7 @@
 def setenv()
 {
-  devops.aws_config("AWS_DESA_CMF")
+  //devops.aws_config("AWS_DESA_CMF")
+  devops.aws_config("705437fe-118c-4fbc-af26-595cbdc1e752")
   env.ECR_URL="104455529394.dkr.ecr.us-east-1.amazonaws.com"
   env.ECR_ID="arch/jenkins-slave-centos"
   env.TAG1="latest"
