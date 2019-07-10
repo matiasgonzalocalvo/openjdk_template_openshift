490f7b8a7a7c38a17891fa53d3cc5125cfa421aa
calvomatias <matias.calvo@comafi.com.ar>
2019-07-10 13:52:22 -0300

    new Dockerfile slave jenkins centos

--- environment/docker/arch-jenkins-slave-centos.groovy
+++ environment/docker/arch-jenkins-slave-centos.groovy
@@ -1,3 +1,7 @@
-env.ECR_URL="104455529394.dkr.ecr.us-east-1.amazonaws.com"
-env.ECR_ID="arch/jenkins-slave-centos"
-env.TAG1="latest"
+def setenv()
+{
+  env.ECR_URL="104455529394.dkr.ecr.us-east-1.amazonaws.com"
+  env.ECR_ID="arch/jenkins-slave-centos"
+  env.TAG1="latest"
+}
+return this;
