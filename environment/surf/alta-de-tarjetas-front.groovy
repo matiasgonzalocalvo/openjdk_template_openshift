c7c8f0a7827dcfb5db776a58b933bd1ae877bf7d
Fabio Pedro Palumbo <fabio.palumbo@comafi.com.ar>
2019-07-11 17:43:29 +0000

    alta-de-tarjetas-front.groovy edited with cert

--- environment/surf/alta-de-tarjetas-front.groovy
+++ environment/surf/alta-de-tarjetas-front.groovy
@@ -65,7 +65,7 @@ def setenv(def cuenta="null")
     env.STACK="${ENV}-INFRA-ADTC-FRONT"
     env.PROJECT="ALTA-DE-TARJETAS"
     env.DEFAULT_BUCKET="${ENV}-${ENV}-comercios-deploy-${ENV}"
-    env.CERT_ARN="arn:aws:acm:us-east-1:104455529394:certificate/a35d91f8-2b7f-4833-8af6-e2f5f5e54c23"
+    env.CERT_ARN="arn:aws:acm:us-east-1:780666439417:certificate/6c9bb0c4-55d1-4026-afb2-3a3e72b9632c"
     env.parameter_overrides="Environment=${ENV} DeployBucket=${BUCKET} HostedZoneName=${DOMAIN} CertArn=$CERT_ARN StackName=${STACK}"
   }
   else
