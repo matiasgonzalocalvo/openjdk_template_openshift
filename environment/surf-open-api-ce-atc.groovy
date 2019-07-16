93cb27f2cf57a36ecbbefb38f9bc72b54c753190
Fabio Pedro Palumbo <fabio.palumbo@comafi.com.ar>
2019-07-16 19:57:07 +0000

    surf-open-api-ce-atc.groovy added dev ENVS

--- environment/surf-open-api-ce-atc.groovy
+++ environment/surf-open-api-ce-atc.groovy
@@ -30,6 +30,18 @@ def setenv(def cuenta="null")
   else if (env.BRANCH_NAME == "develop" || env.BRANCH_NAME == "developjenkinsfile" )
   {
     sh 'echo "$(date) : Seteando variables - BRANCH = ${BRANCH_NAME}"'
+    devops.aws_config("AWS_DESA_CMF")
+    //devops.aws_config("AWS_DESA")
+    env.ENV='dev'
+    env.BUCKET="${ENV}-comafi-base-deploy"
+    env.STACK="${ENV}-INFRA-OPEN-API-CE-ATC"
+    env.STACK_SECURITY="${ENV}-SECURITY-OPEN-API-CE-ATC"
+    env.PROJECT="OPEN-API-CE-ATC"
+    //# VARIABLES ESPECIFICAS
+    env.URL_API='https://dev.desa-comafidigital.com/api'
+    env.TOKEN_API='pendiente'    
+    env.parameter_overrides="UUID=${random} Environment=${ENV} DeployBucket=${BUCKET} StackName=${STACK} UrlApi=${URL_API} TokenApi=${TOKEN_API}"
+    env.parameter_overrides_security="Environment=${ENV}"
     return false
   }
   else if (env.BRANCH_NAME =~ "feature/*" || env.BRANCH_NAME =~ "bugfix/*")
