5b402de69bcb10b05c06252d59170e411a959390
calvomatias <matias.calvo@comafi.com.ar>
2019-07-19 11:02:53 -0300

    first commit test version

--- flujos/devops-flujo.groovy
+++ flujos/devops-flujo.groovy
@@ -7,7 +7,7 @@ def flujo()
   }
   catch (e)
   {
-    node_docker="jenkins-slave-comafi-maven3.3.9-redis"
+    node_docker="master"
   }
   node ("${node_docker}")
   {
@@ -26,10 +26,9 @@ def flujo()
             loadvar.setenv()
           }
         }
-        stage("test devops flow")
+        stage("updateando la version")
         {
-          echo "probando flow"
-          sh "export"
+          devops.update_version()
         }
       }
       catch (e) 
